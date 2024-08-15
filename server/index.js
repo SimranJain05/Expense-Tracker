cc6c707d46fafcf601efe3f9fbaee9a493111cd7
SimranJain05 <114878093+SimranJain05@users.noreply.github.com>
2024-08-16 01:14:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['https://expense-tracker-frontend-simran-jains-projects.vercel.app'],
+    origin: *,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
