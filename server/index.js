76a93eb7fe066aa7e4733d871f846a6a086837a6
SimranJain05 <114878093+SimranJain05@users.noreply.github.com>
2024-08-16 00:12:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: process.env.CLIENT_URL,
     // origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
